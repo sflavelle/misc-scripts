be49ccd50c30f6e7ef0f66407c06b65d544a77f7
Lily Flavelle <me@neurario.com>
2023-03-09 19:18:34 +1100

    Watchstream now also supports vod channels It's 'live' if there was a video in the last day or two

--- watchstream.py
+++ watchstream.py
@@ -2,6 +2,9 @@
 
 import requests
 import streamlink
+import yt_dlp
+from yt_dlp.utils import DateRange
+from datetime import date, datetime, timedelta
 import inquirer
 from rofi import Rofi
 import os
@@ -12,9 +15,20 @@ streamlist = []
 
 r = Rofi()
 tty = False
+today = date.today()
+yesterday = today - timedelta(days=1)
+ydl_opts = {
+        'lazy_playlist': True,
+        'quiet': True,
+        'simulate': True,
+        'playlist_items': '1',
+        'daterange': DateRange(yesterday.strftime("%Y%m%d"), today.strftime("%Y%m%d"))
+        }
+
 
 twitchlink = "https://twitch.tv/{}"
 youtubelink = "https://youtube.com/{}"
+ytvodlink = "https://youtube.com/{}/videos"
 
 conf = open("/home/lily/.config/livestreams.conf", "r")
 if sys.stdin and sys.stdin.isatty():
@@ -34,6 +48,14 @@ for l in conf:
             check = streamlink.streams(youtubelink.format(link[1]))
             if bool(check):
                 streamlist.append(youtubelink.format(link[1]))
+        case 'ytvod':
+            # For a VOD, we fake a 'live' check by instead
+            # Returning a video from today
+            check = None
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                check = ydl.extract_info(ytvodlink.format(link[1]), download=False)
+            if bool(check['entries'][0]):
+                streamlist.append(ytvodlink.format(link[1]))
 # Select from List
 if sys.stdin and sys.stdin.isatty():
     # Running in a terminal, use inquirer
@@ -48,7 +70,12 @@ if sys.stdin and sys.stdin.isatty():
 else:
     selectedstream,selectedindex = r.select('Pick a live stream to watch:', streamlist)
 
-if tty:
-    os.execl('/usr/bin/streamlink', 'streamlink', str(selectedstream['live']), '720p,480p,best')
+url = None
+if "/videos" in str(selectedstream['live']):
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        meta = ydl.extract_info(str(selectedstream['live']), download=False)
+        url = meta['entries'][0]['webpage_url']
+        print(url)
+    os.execl('/usr/bin/mpv', 'mpv', url)
 else:
-    os.spawnl('/usr/bin/streamlink', 'streamlink', str(selectedstream['live']), '720p,480p,best')
+    os.execl('/usr/bin/streamlink', 'streamlink', str(selectedstream['live']), '720p,480p,best')
